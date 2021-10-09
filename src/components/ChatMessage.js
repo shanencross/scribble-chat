d3d2a7abe8218d4497af7a17c241494546a12f2b
Shanen Cross <shanencross@yahoo.com>
2021-10-09 13:25:26 -0700

    Make messages display temporary output

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ScribbleCanvas from './ScribbleCanvas';
 
 function ChatMessage({message={}}) {
   return (
     <React.Fragment>
-      
+      <p><em>{message.user}</em></p>
+      <ScribbleCanvas width={500} height={200}/>
     </React.Fragment>
   );
 }
