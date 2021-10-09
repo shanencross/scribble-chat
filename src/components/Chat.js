722acdbf63f175244cd45c9f1c42199d3f72dd03
Shanen Cross <shanencross@yahoo.com>
2021-10-09 13:24:50 -0700

    Add seeded chat message template

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,10 +2,25 @@ import React from 'react';
 import ChatInputBox from './ChatInputBox';
 import ChatMessageList from './ChatMessageList';
 
+const chatMessages = [
+  {
+    user: "John117",
+    scribbleDrawing: {},
+  },
+  {
+    user: "ssjVegeta99",
+    scribbleDrawing: {},
+  },
+  {
+    user: "MetroidGamer94",
+    scribbleDrawing: {},
+  }
+]
+
 function Chat() {
   return (
     <React.Fragment>
-      <ChatMessageList/>
+      <ChatMessageList chatMessages={chatMessages}/>
       <ChatInputBox/>
     </React.Fragment>
   );
